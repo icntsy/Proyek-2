a7a7fbb45f26efc3bbdfba41cac06cc61fc7fdfc
icntsy <icanatasya72@gmail.com>
2021-11-20 19:02:56 +0700

    789

--- Proyek-2/resources/views/template/layout.blade.php
+++ Proyek-2/resources/views/template/layout.blade.php
@@ -26,7 +26,7 @@
       <!-- Brand -->
       <div class="sidenav-header  align-items-center">
         <a class="navbar-brand" href="javascript:void(0)">
-          <img src="{{ asset('/admin') }}/assets/img/brand/blue.png" class="navbar-brand-img" alt="...">
+          <img src="{{ asset('/admin') }}/assets/img/brand/blue.jpg" class="navbar-brand-img" alt="...">
         </a>
       </div>
       <div class="navbar-inner">
@@ -335,30 +335,30 @@
     <div class="container-fluid mt--6">
       <!-- Footer -->
       <footer class="footer pt-0">
-        <div class="row align-items-center justify-content-lg-between">
-          <div class="col-lg-6">
-            <div class="copyright text-center  text-lg-left  text-muted">
-              &copy; 2020 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
-            </div>
-          </div>
-          <div class="col-lg-6">
-            <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-              <li class="nav-item">
-                <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
-              </li>
-              <li class="nav-item">
-                <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
-              </li>
-              <li class="nav-item">
-                <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
-              </li>
-              <li class="nav-item">
-                <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </footer>
+              <div class="row align-items-center justify-content-lg-between">
+                <div class="col-lg-6">
+                  <div class="copyright text-center  text-lg-left  text-muted">
+                    &copy; 2021 <a href="" class="font-weight-bold ml-1" >Proyek 2</a>
+                  </div>
+                </div>
+                <div class="col-lg-6">
+                  <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                    <li class="nav-item">
+                      <a href="" class="nav-link" >Proyek 2</a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="" class="nav-link" >Kelompok 2</a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="" class="nav-link" >Sisdayu</a>
+                    </li>
+                    <!-- <li class="nav-item">
+                      <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
+                    </li> -->
+                  </ul>
+                </div>
+              </div>
+            </footer>
     </div>
   </div>
   <!-- Argon Scripts -->
